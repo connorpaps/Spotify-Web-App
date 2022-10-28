a84b6f433b4507b952fdef83a06de20cc40b8feb
connorpaps <43863736+connorpaps@users.noreply.github.com>
2022-10-28 16:32:10 -0400

    updated env api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   clientID: "533409e897ea4441af7bba0c73afa288",
   clientSecret: "f0e82186d8db4f9f96129f148c239a14",
-  userAPIBase: "https://polar-coast-19290.herokuapp.com/api/user"
+  userAPIBase: "https://protected-escarpment-74476.herokuapp.com/api/user"
 };
 
 /*
